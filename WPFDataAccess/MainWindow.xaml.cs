882427c582941a47c1f0072cea73d17a77929e74
Perri Demopulos <perri.demopulos@ou.edu>
2019-03-28 17:14:14 -0500

    3-28-19

---
+++ WPFDataAccess/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Perri Demopulos
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFDataAccess
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            //https://www.youtube.com/watch?v=RJvKaHPc8Dc
+            DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+            if ()
+            {
+                var cust = db.Customers.OrderBy(x => x.FirstName);
+            }
+            else if ()
+            {
+                var city = db.Customers.OrderBy(x => x.City);
+            }
+            
+
+        }
+    }
+}
